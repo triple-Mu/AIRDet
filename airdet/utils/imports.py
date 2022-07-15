e9cad0b387b16bc7492aaafa7807b686a03f4ccd
tripleMu <gpu@163.com>
2022-07-15 23:19:07 +0800

    Fix order

--- airdet/utils/imports.py
+++ airdet/utils/imports.py
@@ -3,7 +3,7 @@ import torch
 import sys
 PY37 = sys.version_info[0] == 3 and sys.version_info[1] >= 7
 
-if PY37 or getattr(torch._six, 'PY37', False):
+if getattr(torch._six, 'PY37', False) or PY37:
     import importlib
     import importlib.util
     import sys
